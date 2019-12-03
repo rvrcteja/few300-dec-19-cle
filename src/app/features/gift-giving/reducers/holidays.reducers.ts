4cc3f8bbeb1b74bdeca421fee7d5b7abc7583ee0
rvrcteja <rvrcteja@users.noreply.github.com>
2019-12-03 18:52:34 +0000

    commit day 1 @1:52

--- src/app/features/gift-giving/reducers/holidays.reducers.ts
+++ src/app/features/gift-giving/reducers/holidays.reducers.ts
@@ -13,7 +13,14 @@ export interface HolidayState extends EntityState<HolidayEntity> {
 
 export const adapter = createEntityAdapter<HolidayEntity>();
 
-const initialState = adapter.getInitialState();
+// const initialState = adapter.getInitialState();
+const initialState: HolidayState = {
+  ids: ['1', '2'],
+  entities: {
+    1: { id: '1', name: 'Christmas', date: new Date(2019, 11, 25).toISOString() },
+    2: { id: '2', name: 'Jeff\'s Birthday', date: new Date(2020, 3, 20).toISOString() }
+  }
+};
 
 const reducerFunction = createReducer(
   initialState
--- src/app/features/gift-giving/reducers/index.ts
+++ src/app/features/gift-giving/reducers/index.ts
@@ -1,11 +1,12 @@
 export const featureName = 'giftGivingFeature';
 import * as fromHolidays from './holidays.reducers';
+import { ActionReducerMap } from '@ngrx/store';
 
 export interface GiftGivingState {
   holidays: fromHolidays.HolidayState;
 
 }
 
-export const reducers = {
+export const reducers: ActionReducerMap<GiftGivingState> = {
   holidays: fromHolidays.reducer
 };
